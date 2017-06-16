9e979cd527f0cce707aa7575f8b71c20c1c8a9df
Gideon Ogilvie <gideonogilvie@gmail.com>
2017-06-16 15:50:32 +0200

    added OpenTK
    start of diffuse lightning

--- shaders/fs.glsl
+++ shaders/fs.glsl
@@ -3,6 +3,8 @@
 // shader input
 in vec2 uv;						// interpolated texture coordinates
 in vec4 normal;					// interpolated normal
+in float diffuse;
+
 uniform sampler2D pixels;		// texture sampler
 
 // shader output
@@ -11,5 +13,6 @@ out vec4 outputColor;
 // fragment shader
 void main()
 {
-    outputColor = texture( pixels, uv ) + 0.5f * vec4( normal.xyz, 1 );
+	
+    outputColor = texture( pixels, uv ) * diffuse;
 }
